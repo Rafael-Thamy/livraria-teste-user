fbe55fb233774a5795e7303056616b920da01fc7
Rafael-Thamires <mnmrafa@gmail.com>
2021-10-06 12:57:01 -0300

    changes on server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/apiBookstore-front'));
+app.use(express.static('./docs'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/apiBookstore-front/'}),
+    res.sendFile('index.html', {root: 'docs/'}),
 );
 
 // Start the app by listening on the default Heroku port
